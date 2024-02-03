b354bba99f8eed4545a409d8d179027b0b19d2d1
Oleksandr Kolomiiets <alexcrimson0818@gmail.com>
2024-02-04 01:48:05 +0200

    Add POST for creating new user

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,5 +1,10 @@
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import User
+import json
+import os
 
+@csrf_exempt
 def index(request):
     """
     Main page view
@@ -14,3 +19,40 @@ def index(request):
         }
     }
     return JsonResponse(endpoints)
+
+@csrf_exempt
+def create_entity(request):
+    """
+    Create a new entity via POST request and save data in JSON file
+    """
+    try:
+        if request.method == 'POST':
+            data = json.loads(request.body)
+            username = data.get('username', None)
+            email = data.get('email', None)
+            password = data.get('password', None)
+
+            if username and email and password:
+                entity = {
+                    'username': username,
+                    'email': email,
+                    'password': password
+                }
+                save_entity_to_json(entity)
+                return JsonResponse({'message': 'Entity created successfully'}, status=201)
+            else:
+                return JsonResponse({'error': 'Missing parameters'}, status=400)
+        else:
+            return JsonResponse({'error': 'Only POST method allowed'}, status=405)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
+
+def save_entity_to_json(entity):
+    """
+    Save entity data to a JSON file
+    """
+    file_path = '/Users/zakerden1234/Desktop/Web-Development-HW6-Kolomiiets-/myproject/users.json'
+    mode = 'a' if os.path.exists(file_path) else 'w'
+    with open(file_path, mode) as f:
+        json.dump(entity, f)
+        f.write('\n')
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from myapp.views import index
+from myapp.views import index, create_entity
 
 urlpatterns = [
     path('', index, name='index'),
-    path('admin/', admin.site.urls),
+    path('entity/', create_entity, name='create_entity'),
 ]
